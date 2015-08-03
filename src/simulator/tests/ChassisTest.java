5464a38bb9a2aa3501b667a34b20bd445ff607c1
coelhudo <coelhudo@gmail.com>
2015-08-03 12:16:23 -0400

    chassis test created

---
+++ src/simulator/tests/ChassisTest.java
@@ -0,0 +1,26 @@
+package simulator.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import simulator.Environment;
+import simulator.physical.BladeServer;
+import simulator.physical.Chassis;
+
+public class ChassisTest {
+
+    @Test
+    public void testChassisCreation() {
+        Environment environment = new Environment();
+        final int chassisID = 1;
+        Chassis chassis = new Chassis(chassisID, environment);
+        assertEquals(chassisID, chassis.getChassisID());
+        assertEquals(0, chassis.getRackID());
+        List<BladeServer> bladeServers = chassis.getServers();
+        assertTrue(bladeServers.isEmpty());
+    }
+
+}
