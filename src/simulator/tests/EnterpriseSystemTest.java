c144676ee441a277299164a0fa8923f756394cad
coelhudo <coelhudo@gmail.com>
2015-08-17 12:16:11 -0400

    runAcycle test created

--- src/simulator/tests/EnterpriseSystemTest.java
+++ src/simulator/tests/EnterpriseSystemTest.java
@@ -1,11 +1,18 @@
 package simulator.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
-import java.io.BufferedReader;
 
 import simulator.Environment;
 import simulator.SLAViolationLogger;
@@ -15,6 +22,8 @@ import simulator.system.EnterpriseSystemPOD;
 import simulator.system.SystemPOD;
 
 public class EnterpriseSystemTest {
+    
+    public static final String FAIL_ERROR_MESSAGE = "This was not supposed to happen";
 
     @Test
     public void testEnterpriseSystemCreation() { 
@@ -44,5 +53,31 @@ public class EnterpriseSystemTest {
         assertEquals(0, enterpriseSystem.getSLAviolation());
         
         verifyNoMoreInteractions(mockedEnvironment, mockedDataCenter, mockedSLAViolationLogger);    
+    }
+    
+    @Test
+    public void testRunACycle_WithoutApplication () {
+        SystemPOD systemPOD = new EnterpriseSystemPOD();
+        Environment mockedEnvironment = mock(Environment.class);
+        DataCenter mockedDataCenter = mock(DataCenter.class);
+        SLAViolationLogger mockedSLAViolationLogger = mock(SLAViolationLogger.class);
+        EnterpriseSystem enterpriseSystem = EnterpriseSystem.Create(systemPOD, mockedEnvironment, mockedDataCenter, mockedSLAViolationLogger);
+        
+        try {
+            Method runACycle = EnterpriseSystem.class.getDeclaredMethod("runAcycle");
+            runACycle.setAccessible(true);
+            boolean result = (boolean) runACycle.invoke(enterpriseSystem);
+            assertTrue(result);
+        } catch (NoSuchMethodException e) {
+            fail(FAIL_ERROR_MESSAGE);
+        } catch (SecurityException e) {
+            fail(FAIL_ERROR_MESSAGE);
+        } catch (IllegalAccessException e) {
+            fail(FAIL_ERROR_MESSAGE);
+        } catch (IllegalArgumentException e) {
+            fail(FAIL_ERROR_MESSAGE);
+        } catch (InvocationTargetException e) {
+            fail(FAIL_ERROR_MESSAGE);
+        }
     }
 }
