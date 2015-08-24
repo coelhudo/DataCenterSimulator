2055bc160043252a8030062b23ed2b9e842ccf5c
coelhudo <coelhudo@gmail.com>
2015-08-24 01:21:29 -0400

    improving integration test

--- src/simulator/tests/integration_tests/ReadingConfigurationIT.java
+++ src/simulator/tests/integration_tests/ReadingConfigurationIT.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -39,12 +41,12 @@ public class ReadingConfigurationIT {
         assertEquals(50, chassisSet.size());
         int chassisID = 0;
         Set<Integer> rackIDSet = new HashSet<Integer>();
-        for(Chassis chassis : chassisSet) {
+        for (Chassis chassis : chassisSet) {
             assertEquals(chassisID, chassis.getChassisID());
             chassisID++;
             rackIDSet.add(chassis.getRackID());
             assertEquals(1, chassis.getServers().size());
-            for(BladeServer bladeServer : chassis.getServers()) {
+            for (BladeServer bladeServer : chassis.getServers()) {
                 assertEquals(chassis.getChassisID(), bladeServer.getChassisID());
                 assertEquals(chassis.getRackID(), bladeServer.getRackId());
                 assertEquals(chassis.getChassisID(), bladeServer.getServerID());
@@ -62,32 +64,55 @@ public class ReadingConfigurationIT {
             }
         }
         assertEquals(10, rackIDSet.size());
-        
+
         Systems systems = simulator.getSystems();
         List<ComputeSystem> computeSystems = systems.getComputeSystems();
         List<EnterpriseSystem> enterpriseSystems = systems.getEnterpriseSystems();
         List<InteractiveSystem> interactiveSystems = systems.getInteractiveSystems();
+
+        DataCenterAM dataCenterAM = dataCenter.getAM();
+        assertFalse(dataCenterAM.isSlowDownFromCooler());
+        assertEquals(0, dataCenterAM.getBlockTimer());
 
         assertEquals(0.0, dataCenter.getTotalPowerConsumption(), 1.0E-8);
         assertFalse(computeSystems.isEmpty());
         assertFalse(enterpriseSystems.isEmpty());
         assertTrue(interactiveSystems.isEmpty());
         assertEquals(1, computeSystems.size());
-        assertEquals(1, enterpriseSystems.size());
-        
-        DataCenterAM dataCenterAM = dataCenter.getAM();
-        assertFalse(dataCenterAM.isSlowDownFromCooler());
-        assertEquals(0, dataCenterAM.getBlockTimer());
-        
+
         ComputeSystem computeSystem = computeSystems.get(0);
+
+        Set<Integer> serversInComputeSystem = new HashSet<Integer>(Arrays.asList(0, 2, 3, 4, 5, 6, 7, 8, 9, 21, 20, 23,
+                22, 25, 24, 27, 26, 29, 28, 35, 38, 39, 36, 37, 42, 43, 40, 41, 46, 47, 44, 45, 49, 48));
+        Set<Integer> chassisInComputeSystem = new HashSet<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 21, 20,
+                23, 22, 25, 24, 27, 26, 29, 28, 35, 38, 39, 36, 37, 42, 43, 40, 41, 46, 47, 44, 45, 49, 48));
+        Set<Integer> racksInComputeSystem = new HashSet<Integer>(Arrays.asList(0, 1, 4, 5, 7, 8, 9));
+        for (BladeServer bladeServer : computeSystem.getComputeNodeList()) {
+            chassisInComputeSystem.remove(bladeServer.getChassisID());
+            racksInComputeSystem.remove(bladeServer.getRackId());
+            serversInComputeSystem.remove(bladeServer.getServerID());
+        }
+        assertTrue(serversInComputeSystem.isEmpty());
+        assertTrue(chassisInComputeSystem.isEmpty());
+        assertTrue(racksInComputeSystem.isEmpty());
+
         assertFalse(computeSystem.isDone());
         assertEquals(0, computeSystem.getAccumolatedViolation());
         assertEquals("HPC_First", computeSystem.getName());
         assertFalse(computeSystem.getComputeNodeIndex().isEmpty());
         assertEquals(35, computeSystem.getComputeNodeIndex().size());
         assertFalse(computeSystem.getComputeNodeList().isEmpty());
-        assertEquals(35, computeSystem.getComputeNodeList().size()); //TODO: check how the last two lists are correlated.
-        assertEquals(35, computeSystem.numberOfRunningNode()); //FIXME: is this correct? Check Active (below) vs Running
+        assertEquals(35, computeSystem.getComputeNodeList().size()); // TODO:
+                                                                     // check
+                                                                     // how the
+                                                                     // last two
+                                                                     // lists
+                                                                     // are
+                                                                     // correlated.
+        assertEquals(35, computeSystem.numberOfRunningNode()); // FIXME: is this
+                                                               // correct? Check
+                                                               // Active (below)
+                                                               // vs Running
         assertEquals(0, computeSystem.numberOfIdleNode());
         assertEquals(0, computeSystem.getNumberOfActiveServ());
         assertEquals(35, computeSystem.getNumberOfNode());
@@ -96,8 +121,22 @@ public class ReadingConfigurationIT {
         assertFalse(computeSystem.isBlocked());
         assertNotEquals(null, computeSystem.getResourceAllocation());
         assertNotEquals(null, computeSystem.getScheduler());
-        
+
+        assertEquals(1, enterpriseSystems.size());
         EnterpriseSystem enterpriseSystem = enterpriseSystems.get(0);
+        
+        Set<Integer> serversInEnterpriseSystem = new HashSet<Integer>(Arrays.asList(17, 19, 18, 10, 11, 12, 13, 14));
+        Set<Integer> chassisInEnterpriseSystem = new HashSet<Integer>(Arrays.asList(17, 19, 18, 10, 11, 12, 13, 14));
+        Set<Integer> racksInEnterpriseSystem = new HashSet<Integer>(Arrays.asList(2,3));
+        for (BladeServer bladeServer : enterpriseSystem.getComputeNodeList()) {
+            chassisInEnterpriseSystem.remove(bladeServer.getChassisID());
+            racksInEnterpriseSystem.remove(bladeServer.getRackId());
+            serversInEnterpriseSystem.remove(bladeServer.getServerID());
+        }
+        assertTrue(serversInEnterpriseSystem.isEmpty());
+        assertTrue(chassisInEnterpriseSystem.isEmpty());
+        assertTrue(racksInEnterpriseSystem.isEmpty());
+        
         assertFalse(enterpriseSystem.isDone());
         assertEquals(0, enterpriseSystem.getAccumolatedViolation());
         assertEquals("Enterprise_01", enterpriseSystem.getName());
@@ -112,6 +151,6 @@ public class ReadingConfigurationIT {
         assertEquals(0, enterpriseSystem.getSLAviolation());
         assertNotEquals(null, enterpriseSystem.getResourceAllocation());
         assertNotEquals(null, enterpriseSystem.getScheduler());
-        
+
     }
 }
