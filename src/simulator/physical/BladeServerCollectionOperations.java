7413f455568fa93c7f98e712bd68f8e5b5744769
coelhudo <coelhudo@gmail.com>
2015-08-27 21:59:51 -0400

    removed unused variable

--- src/simulator/physical/BladeServerCollectionOperations.java
+++ src/simulator/physical/BladeServerCollectionOperations.java
@@ -1,12 +1,9 @@
 package simulator.physical;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 public final class BladeServerCollectionOperations {
 
-    public static final Logger LOGGER = Logger.getLogger(BladeServerCollectionOperations.class.getName());
-    
     public static void runAll(List<BladeServer> bladeServers) {
         for (BladeServer bladeServer : bladeServers) {
             bladeServer.run();
@@ -20,7 +17,7 @@ public final class BladeServerCollectionOperations {
         }
         return result;
     }
-    
+
     public static double totalResponseTime(List<BladeServer> bladeServers) {
         double result = 0;
         for (BladeServer bladeServer : bladeServers) {
