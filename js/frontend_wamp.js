022d2a55a3e228e90df9fc15508fdbf9d8a88327
coelhudo <coelhudo@gmail.com>
2015-10-02 12:16:56 -0400

    working on the frontend; ignoring unix temporary file

--- js/frontend_wamp.js
+++ js/frontend_wamp.js
@@ -10,25 +10,60 @@ var connection = new autobahn.Connection({
     realm: 'crossbardemo'}
 					);
 
+var currentSession = null
+
 connection.onopen = function (session) {
 
     var received = 0;
 
 //     function onevent1(args) {
-// 	console.log("Got event:", args[0]);
-// 	received += 1;
-// 	if (received > 5) {
+//	console.log("Got event:", args[0]);
+//	received += 1;
+//	if (received > 5) {
 //             console.log("Closing ..");
 //             connection.close();
-// 	}
-//     }
+//	}
+//     }console.log(racks[i])
 
     //session.subscribe('com.myapp.topic1', onevent1);
-    session.call('digs.sim.topology').then(
+    currentSession = session
+};
+
+function execute() {
+    if(!currentSession) {
+	return
+    }
+
+    createDataCenterElement = function(elementUID, dataCenterClass) {
+	var HTMLDataCenterElementFormatted = document.createElement("div")
+	HTMLDataCenterElementFormatted.setAttribute('id', elementUID)
+	HTMLDataCenterElementFormatted.setAttribute('class', dataCenterClass)
+	return HTMLDataCenterElementFormatted
+    };
+
+    currentSession.call('digs.sim.topology').then(
 	function(res) {
-	    $("#main").append(JSON.stringify(res));
+	    var topology = JSON.parse(res)
+	    var racks = topology.dataCenter.sort(function(a,b) {
+		return parseInt(a.id.replace(/\./g, "")) > parseInt(b.id.replace(/\./g, ""))
+	    })
+	    for(i = 0; i < racks.length; i++) {
+		var HTMLDataCenterRackFormatted = createDataCenterElement(racks[i].id, 'rack')
+		$("#main").append(HTMLDataCenterRackFormatted)
+		var rackSelector = "#" + racks[i].id.replace(/\./g, "\\.")
+		$(rackSelector).html(racks[i].id)
+		$(rackSelector).css({"padding-left":"50px"});
+		var chassis = racks[i].chassis
+		for(j = 0; j < chassis.length; j++) {
+		    var HTMLDataCenterChassisFormatted = createDataCenterElement(chassis[j].id, 'chassis')
+		    $(rackSelector).append(HTMLDataCenterChassisFormatted)
+		    var chassisSelector = "#" + chassis[j].id.replace(/\./g, "\\.")
+		    $(chassisSelector).html(chassis[j].id)
+		    $(chassisSelector).css({"padding-left":"75px"});
+		}
+	    }
 	}
     );
-};
+}
 
 connection.open();
