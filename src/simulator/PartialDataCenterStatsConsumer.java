7334aa8c84fd29aa218bc63d5126e93d8cdfa2c5
coelhudo <coelhudo@gmail.com>
2016-01-08 16:15:08 -0500

    submiting docs related to the simulator; partial results being logged

--- src/simulator/PartialDataCenterStatsConsumer.java
+++ src/simulator/PartialDataCenterStatsConsumer.java
@@ -1,18 +1,25 @@
 package simulator;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import com.google.inject.Inject;
 
+import simulator.physical.BladeServer.BladeServerStats;
+import simulator.physical.Chassis.ChassisStats;
 import simulator.physical.DataCenter.DataCenterStats;
+import simulator.physical.Rack.RackStats;
 
 public class PartialDataCenterStatsConsumer implements Runnable {
 
     private static final Logger LOGGER = Logger.getLogger(PartialDataCenterStatsConsumer.class.getName());
     private BlockingQueue<DataCenterStats> partialDataCenterStats;
     private int counter = 0;
+    private PrintWriter writer;
 
     @Inject
     public PartialDataCenterStatsConsumer(BlockingQueue<DataCenterStats> partialResults) {
@@ -22,6 +29,7 @@ public class PartialDataCenterStatsConsumer implements Runnable {
     @Override
     public void run() {
         try {
+            writer = new PrintWriter("partial_results.log", "UTF-8");
             while (true) {
                 DataCenterStats result = partialDataCenterStats.poll(100, TimeUnit.MILLISECONDS);
                 if (result != null) {
@@ -35,15 +43,28 @@ public class PartialDataCenterStatsConsumer implements Runnable {
                     break;
                 }
             }
+        } catch (FileNotFoundException ex) {
+            LOGGER.info(ex.getMessage());
+        } catch (UnsupportedEncodingException ex) {
+            LOGGER.info(ex.getMessage());
         } catch (InterruptedException ex) {
             LOGGER.info(ex.getMessage());
         }
-
+        writer.close();
         LOGGER.info("Consumer done");
     }
 
     private synchronized void consume(DataCenterStats result) {
-        // XXX: the view of partial result is being done through webview. So,
-        // this acting as just a dummy data consumer
+        writer.println("===== START =====");
+        for(RackStats rackStats : result.getRacksStats()) {
+            for(ChassisStats chassisStats : rackStats.getChassisStats()) {
+                for(BladeServerStats bladeServerStats : chassisStats.getBladeServersStats()) {
+                        writer.println(String.format("%s %.1f %.1f", bladeServerStats.getStatus(),
+                                              bladeServerStats.getCurrentCPU(),
+                                              bladeServerStats.getMIPS()));
+                }
+            }
+        }
+        writer.println("===== END =====");
     }
 }
